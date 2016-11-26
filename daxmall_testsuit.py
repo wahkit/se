d40ef00e165f129d597249350ec01eff65539d9e
wahkit <891788456@qq.com>
2016-11-26 10:27:20 +0800

    a
    a

---
+++ daxmall_testsuit.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+import unittest, doctest
+import normalorder
+import cancel_order
+import haiwaigou
+import login_out
+import shopManage
+
+import HTMLTestRunner
+
+suite=doctest._DocTestSuite()
+suite.addTest(unittest.makeSuite(normalorder.Normalorder))
+suite.addTest(unittest.makeSuite(cancel_order.Cancel_order))
+suite.addTest(unittest.makeSuite(haiwaigou.Haiwaigou))
+suite.addTest(unittest.makeSuite(login_out.Login_out))
+suite.addTest(unittest.makeSuite(shopManage.ShopManage))
+
+
+
+filename = ('D:\\daxmall.html')
+fp = open(filename, 'wb')
+runner=HTMLTestRunner.HTMLTestRunner(
+                                    stream=fp,
+                                    title='Report_title',
+                                    description='Report_description')
+runner.run(suite)
+fp.close()
